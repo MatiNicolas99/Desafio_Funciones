46d38eda10bf89e915a1b9d52f53d9e504d21882
MatiNicolas99 <103612186+MatiNicolas99@users.noreply.github.com>
2022-06-23 21:23:34 -0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,12 @@
 
-let verificador = true;
-let ele = document.getElementById("ele1")
-ele.style.backgroundColor = 'green';
-
-ele.addEventListener("click", function pintar(color = "yellow") {
-
-    if (color = "yellow" && verificador === true) {
-        ele.style.backgroundColor = "green";
-        verificador = false;
-    }
-    else {
-        ele.style.backgroundColor = "yellow";
-        verificador = true;
-    }
+let ele = document.getElementById("ele1");
+ele.style.backgroundColor = "green"
+ele.addEventListener("click", function () {
+    pintar(ele)
 });
+
+function pintar(elemento, color="yellow") {
+        elemento.style.backgroundColor = color;
+}
+
+
